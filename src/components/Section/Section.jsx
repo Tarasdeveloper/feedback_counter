a0162e8bb8e6944426d861f8a8e6df260c8c2760
Tarasdeveloper <107717399+Tarasdeveloper@users.noreply.github.com>
2023-11-23 15:33:57 +0000

    Deploying to gh-pages from @ Tarasdeveloper/feedback_counter@65c4b907024023f25994bb419f5893a51d4fbbfc 🚀

--- src/components/Section/Section.jsx
+++
@@ -1,8 +0,0 @@
-import { SectionMain } from './Section.styled';
-
-export const Section = ({ title, children }) => (
-  <SectionMain>
-    <h2>{title}</h2>
-    {children}
-  </SectionMain>
-);
--- src/components/Statistics/Statistics.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const StatList = styled.ul`
-  li {
-    font-size: 22px;
-    margin-bottom: 5px;
-  }
-`;
